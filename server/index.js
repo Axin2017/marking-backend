6ed8ec3b5c10cd1d40cfe7a1fe34c88e7c1e153c
XinTan <838524739@qq.com>
2019-05-07 11:37:57 +0800

    修改数据结构

--- server/index.js
+++ server/index.js
@@ -11,19 +11,29 @@ app.use(async (ctx, next) => {
   ctx.set('Access-Control-Allow-Origin','*');
   await next();
 })
-// 统一错误处理
-app.on("error", (err, ctx) => {
-  ctx.response.status = err.statusCode || err.status || 500;
-  ctx.response.body = {
-    message: err.message
-  };
-});
+// 拦截请求信息
+app.use(async (ctx,next)=>{
+  const query=ctx.query
+  query && console.log(`query=${JSON.stringify(query)}`)
+  await next();
+})
+
 // 注册路由
 registRouter(router);
 // 在router之前加上
 app.use(bodyParser());
 // 路由
 app.use(router.routes());
+
+// 统一错误处理
+app.on("error", (err, ctx) => {
+  console.error("server error:\n"+err)
+  ctx.response.status = err.statusCode || err.status || 500;
+  ctx.response.body = {
+    message: err.message
+  };
+});
+
 
 app.listen(8081);
 console.log("app started at port 8081...");
